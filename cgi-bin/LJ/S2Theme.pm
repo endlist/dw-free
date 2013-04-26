be975fa2c18986da6e8074d02e2bf0e621d86346
Lian <wiring@fastmail.fm>
2013-04-26 05:45:01 +0000

    Initial commit for style Indexed

--- cgi-bin/LJ/S2Theme.pm
+++ cgi-bin/LJ/S2Theme.pm
@@ -58,6 +58,7 @@ sub default_themes {
         funkycircles => 'funkycircles/darkpurple',
         hibiscus => 'hibiscus/tropical',
         headsup => 'headsup/caturdaygreytabby',
+        indexed => 'indexed/spotcolor',
         leftovers => 'leftovers/fruitsalad',
         lefty => 'lefty/greenmachine',
         librariansdream => 'librariansdream/grayscalelight',
---
+++ cgi-bin/LJ/S2Theme/indexed.pm
@@ -0,0 +1,8 @@
+package LJ::S2Theme::indexed;
+use base qw( LJ::S2Theme );
+use strict;
+
+sub layouts { ( "3l" => "three-columns-left" ) }
+sub layout_prop { "layout_type" }
+
+1;
